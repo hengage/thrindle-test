6d9d05fcc897c5675f15a0a9d2f4f94bd9344f51
hengage <tunecarterkillz@gmail.com>
2023-11-22 01:40:33 +0100

    Refactor: get customer by phone number

--- api/components/users/services/user.service.ts
+++ api/components/users/services/user.service.ts
@@ -3,47 +3,69 @@ import { User } from "../models/users.models";
 import { ICreateuser, ILoginUser, IUser } from "../users.interface";
 
 class UsersService {
-    public async signup(payload: ICreateuser): Promise<IUser> {
-        try {
-            const user = new User({
-                firstName: payload.firstName,
-                lastName: payload.lastName,
-                phoneNumber: payload.phoneNumber,
-                email: payload.email,
-                password: payload.password,
-            })
+  public async signup(payload: ICreateuser): Promise<IUser> {
+    try {
+      const user = new User({
+        firstName: payload.firstName,
+        lastName: payload.lastName,
+        phoneNumber: payload.phoneNumber,
+        email: payload.email,
+        password: payload.password,
+      });
 
-            const newUser = await user.save()
-            return newUser
-        } catch (error: any) {
-            throw new HandleException(error.status, error.message)
-        }
+      const newUser = await user.save();
+      return newUser;
+    } catch (error: any) {
+      throw new HandleException(error.status, error.message);
     }
+  }
 
-    public async login (payload: ILoginUser): Promise<IUser> {
-        try {
-            const user = await User.findOne({phoneNumber: payload.phoneNumber})
-            .select('phoneNumber password')
-            .lean()
-            .exec()
+  public async login(payload: ILoginUser): Promise<IUser> {
+    try {
+      const user = await this.getUserByPhoneNumber(
+        payload.phoneNumber, 'phoneNumber password'
+    );
 
-            if ( !user ) {
-                throw new HandleException(STATUS_CODES.NOT_FOUND, 'User not found')
-            }
-            const passwordsMatch = await encryption.compareValues(
-                payload.password,
-                user.password
-              );
-              if (!passwordsMatch) {
-                throw new HandleException(
-                  STATUS_CODES.UNAUTHORIZED,
-                  "Incorrect password"
-                );
-              }
-            return user
-        } catch (error: any) {
-            throw new HandleException(error.status, error.message)
-        }
+      if (!user) {
+        throw new HandleException(STATUS_CODES.NOT_FOUND, "User not found");
+      }
+      const passwordsMatch = await encryption.compareValues(
+        payload.password,
+        user.password
+      );
+
+      if (!passwordsMatch) {
+        throw new HandleException(
+          STATUS_CODES.UNAUTHORIZED,
+          "Incorrect password"
+        );
+      }
+      return user;
+    } catch (error: any) {
+      throw new HandleException(error.status, error.message);
     }
+  }
+
+  public async getUserByPhoneNumber(
+    phoneNumber: string,
+    selectFields?: string
+  ): Promise<IUser> {
+    try {
+      const query = User.findOne({  phoneNumber: { $eq: phoneNumber } })
+
+      if (selectFields) {
+        query.select(selectFields);
+      }
+      const user = await query.lean().exec()
+
+      if (!user) {
+        throw new HandleException(STATUS_CODES.NOT_FOUND, "User not found");
+      }
+
+      return user;
+    } catch (error: any) {
+      throw new HandleException(error.status, error.message);
+    }
+  }
 }
-export const usersService = new UsersService()
+export const usersService = new UsersService();
