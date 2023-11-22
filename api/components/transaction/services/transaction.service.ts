53f98f09712204875be82411c8e615641464027a
hengage <tunecarterkillz@gmail.com>
2023-11-22 20:38:16 +0100

    Chore: transaction

--- api/components/transaction/services/transaction.service.ts
+++ api/components/transaction/services/transaction.service.ts
@@ -12,30 +12,29 @@ class TransactionService {
   public async oneTimeAccountPayment(email: string, amount: number) {
     // Creates a dynamic account number which would expire after payment
     // has been confirmed or the period of time indicated.
-    const txRef = this.transactionRef();
-
-    const response = await axios.post(
-      "https://api.flutterwave.com/v3/virtual-account-numbers",
-      {
-        email,
-        amount,
-        tx_ref: txRef,
-        is_permanent: false,
-        expires: 300,
-        frequency: 3,
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${FLW_SECRET_KEY}`,
+    try {
+      const response = await axios.post(
+        "https://api.flutterwave.com/v3/virtual-account-numbers",
+        {
+          email,
+          amount,
+          tx_ref: this.transactionRef(),
+          is_permanent: false,
+          expires: 300,
+          frequency: 3,
         },
-      }
-    );
+        {
+          headers: {
+            Authorization: `Bearer ${FLW_SECRET_KEY}`,
+          },
+        }
+      );
 
-    const virtualAccount = response.data.data;
-    return virtualAccount;
-  }
-  catch(error: any) {
-    throw new HandleException(error.status, error.response.data.message);
+      const virtualAccount = response.data.data;
+      return virtualAccount;
+    } catch (error: any) {
+      throw new HandleException(error.status, error.response.data.message);
+    }
   }
 }
 
