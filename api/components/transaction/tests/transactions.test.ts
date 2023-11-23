5e200ee2e8c899d0347e548f48a5061f3c3b2e36
hengage <tunecarterkillz@gmail.com>
2023-11-23 17:23:42 +0100

    Feature: test dynamic account creation

--- api/components/transaction/tests/transactions.test.ts
+++ api/components/transaction/tests/transactions.test.ts
@@ -1,16 +1,20 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+
+import axios from "axios";
 
 import { Transaction } from "../models/transaction.models";
-import { MongoMemoryServer } from 'mongodb-memory-server';
-// jest.mock(Transaction); 
-
+import { MongoMemoryServer } from "mongodb-memory-server";
+import { transactionService } from "../services/transaction.service";
+// jest.mock(Transaction);
 
 const mongoServer = new MongoMemoryServer();
-let connectionString: string
+let connectionString: string;
 
-describe('Transaction', () => {
-    let connectSpy: jest.SpyInstance<ReturnType<typeof mongoose.connect>,
-    Parameters<typeof mongoose.connect>>;
+describe("Transaction", () => {
+  let connectSpy: jest.SpyInstance<
+    ReturnType<typeof mongoose.connect>,
+    Parameters<typeof mongoose.connect>
+  >;
 
   beforeAll(async () => {
     jest.setTimeout(100000);
@@ -19,7 +23,7 @@ describe('Transaction', () => {
 
     connectionString = await (mongoServer.getUri() as any);
 
-    mongoose.set('strictQuery', false)
+    mongoose.set("strictQuery", false);
     await mongoose.connect(connectionString);
   });
 
@@ -29,20 +33,39 @@ describe('Transaction', () => {
     await mongoServer.stop();
   });
 
-  test('test document saves to db', async () => {
+  test("test document saves to db", async () => {
     const transactionData = new Transaction({
-        amount: 3000,
-        user: 'allison becker',
-        senderEmail: "allisonbekcer@hotmail.com",
-        reference: "tyt46ugbr",
-        fee: 0,
-      });
-      console.log('Before save operation');
-      await transactionData.save();
-      console.log('After save operation');
-  
-      // Verify that the transaction was saved to the database
-      const foundTransaction = await Transaction.findOne({ reference: 'tyt46ugbr' });
-      expect(foundTransaction).toBeTruthy();
-  })
+      amount: 3000,
+      user: "allison becker",
+      senderEmail: "allisonbekcer@hotmail.com",
+      reference: "tyt46ugbr",
+      fee: 0,
+    });
+    console.log("Before save operation");
+    await transactionData.save();
+    console.log("After save operation");
+
+    // Verify that the transaction was saved to the database
+    const foundTransaction = await Transaction.findOne({
+      reference: "tyt46ugbr",
+    });
+    expect(foundTransaction).toBeTruthy();
+  });
+
+  test("it calls axios.post 1 time", async () => {
+    const mockData = {
+      email: "test_email@email.com",
+      firstName: "first-name",
+      lastName: "last-name",
+      phoneNumber: "phone-number",
+      businessId: "test_business_id",
+    };
+
+    await transactionService.oneTimeAccountPayment(
+      "henry@gmail.com",
+      5000,
+      "123"
+    );
+    expect(axios.post).toHaveBeenCalledTimes(1);
+  });
 });
